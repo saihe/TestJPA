71e793ee4f03d64e078db6c57fcdf6b1be761445
Kyohei Saito <saihe.kyon@gmail.com>
2019-06-16 10:09:00 +0900

    mysql用にブランチ作成

--- jpa/src/main/java/test/jpa/repository/EmployeeRepository.java
+++ jpa/src/main/java/test/jpa/repository/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package test.jpa.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import test.jpa.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
